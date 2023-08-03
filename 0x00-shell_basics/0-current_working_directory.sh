b15fad530a1363eb8110df51cc57b077b2307fb1
musab-omer <mosab.katry@gmail.com>
2023-08-02 17:11:53 -0700

    initla commit

---
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#git the absolute path of the courent working directory 
+current_dir=$(pwd)
+
+#Print the absolute path 
+echo "Current working directory :" $current_dir
---
+++ README.md
@@ -0,0 +1,1 @@
+This repo is about Shell  programming
